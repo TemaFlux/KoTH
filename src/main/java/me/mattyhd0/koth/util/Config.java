17ffd836402c78e4e07b2d066db4a060cccee390
TemaFlux <temaflex1337@gmail.com>
2024-07-29 00:15:28 +0300

    Update Config.java

--- src/main/java/me/mattyhd0/koth/util/Config.java
+++ src/main/java/me/mattyhd0/koth/util/Config.java
@@ -1,31 +1,28 @@
 package me.mattyhd0.koth.util;
 
+import lombok.Getter;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Config {
-
     private static YMLFile configFile;
-    private static YMLFile kothsFile;
-    private static YMLFile messagesFile;
-    private static YMLFile rewardsFile;
-    private static YMLFile scheduleFile;
+    @Getter private static YMLFile kothsFile;
+    @Getter private static YMLFile messagesFile;
+    @Getter private static YMLFile rewardsFile;
+    @Getter private static YMLFile scheduleFile;
 
 
-    public static void loadConfiguration(){
-
+    public static void loadConfiguration() {
         configFile = new YMLFile("config.yml");
         messagesFile = new YMLFile("messages.yml");
         kothsFile = new YMLFile("koths.yml");
         rewardsFile = new YMLFile("koth-rewards.yml");
         scheduleFile = new YMLFile("schedule.yml");
-
     }
     
     public static String getMessage(String message) {
-
         FileConfiguration messages = messagesFile.get();
         String msg = messages.getString(message);
 
@@ -43,52 +40,28 @@ public class Config {
         List<String> coloredList = new ArrayList<>();
 
         if (msgList.size() > 0) {
-
-            for(String line: msgList){
-
+            for (String line: msgList) {
                 line = line.replaceAll("\\{prefix}", messages.getString("messages.prefix"));
                 coloredList.add(Util.color(line));
-
             }
 
             return coloredList;
-
         } else {
-
             List<String> error = new ArrayList<>();
             error.add(org.bukkit.ChatColor.translateAlternateColorCodes('&', "&c[KoTH] Error: Message " + message + " does not exist in messages.yml"));
             return error;
         }
-
     }
     
     public static boolean getBoolean(String path) {
-        boolean bol = configFile.get().getBoolean(path);
         try {
-            return bol;
+            return configFile.get().getBoolean(path);
         } catch (NullPointerException exception) {
             return false;
         }
     }
 
-    public static FileConfiguration getConfig(){
+    public static FileConfiguration getConfig() {
         return configFile.get();
     }
-
-
-    public static YMLFile getKothsFile(){
-        return kothsFile;
-    }
-
-    public static YMLFile getRewardsFile(){
-        return rewardsFile;
-    }
-
-    public static YMLFile getScheduleFile(){
-        return scheduleFile;
-    }
-
-
-
-
 }
