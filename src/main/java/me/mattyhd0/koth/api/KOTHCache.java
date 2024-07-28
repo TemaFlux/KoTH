ceee91668164289ddc841aab412db67ce0beface
TemaFlux <temaflex1337@gmail.com>
2024-07-29 00:15:18 +0300

    Create KOTHCache.java

---
+++ src/main/java/me/mattyhd0/koth/api/KOTHCache.java
@@ -0,0 +1,9 @@
+package me.mattyhd0.koth.api;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class KOTHCache {
+    public static final Map<UUID, Integer> points = new HashMap<>();
+}
